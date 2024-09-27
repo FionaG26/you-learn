e1d56b5fffcb0c23fa056dae2712dfef9928ac51
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-09-27 10:39:41 +0300

    Update Register.js

--- e-learning/src/components/Register.js
+++ e-learning/src/components/Register.js
@@ -17,28 +17,28 @@ const Register = () => {
 
       {/* Navbar Start */}
       <nav className="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-        <a href="index.html" className="navbar-brand d-flex align-items-center px-4 px-lg-5">
+        <button className="navbar-brand d-flex align-items-center px-4 px-lg-5 btn btn-link" onClick={() => window.location.href = 'index.html'}>
           <p className="m-0 fw-bold" style={{ fontSize: '25px' }}>
-            <img src="img/icon.png" alt="" height="50px" /> Secret<span style={{ color: '#fb873f' }}>Coder</span>
+            <img src="img/icon.png" alt="Icon" height="50px" /> Secret<span style={{ color: '#fb873f' }}>Coder</span>
           </p>
-        </a>
+        </button>
         <button className="navbar-toggler me-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" style={{ border: 'none' }}>
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <div className="navbar-nav ms-auto p-4 p-lg-0">
-            <a href="index.html" className="nav-item nav-link active">Home</a>
-            <a href="about.html" className="nav-item nav-link">About</a>
-            <a href="courses.html" className="nav-item nav-link">Courses</a>
+            <button className="nav-item nav-link btn btn-link" onClick={() => window.location.href = 'index.html'}>Home</button>
+            <button className="nav-item nav-link btn btn-link" onClick={() => window.location.href = 'about.html'}>About</button>
+            <button className="nav-item nav-link btn btn-link" onClick={() => window.location.href = 'courses.html'}>Courses</button>
             <div className="nav-item dropdown">
-              <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+              <button className="nav-link dropdown-toggle btn btn-link" data-bs-toggle="dropdown">Pages</button>
               <div className="dropdown-menu fade-down m-0">
-                <a href="team.html" className="dropdown-item">Our Team</a>
-                <a href="testimonial.html" className="dropdown-item">Testimonial</a>
+                <button className="dropdown-item btn btn-link" onClick={() => window.location.href = 'team.html'}>Our Team</button>
+                <button className="dropdown-item btn btn-link" onClick={() => window.location.href = 'testimonial.html'}>Testimonial</button>
               </div>
             </div>
-            <a href="contact.html" className="nav-item nav-link">Contact</a>
-            <a href="login.html" className="nav-item nav-link"><FontAwesomeIcon icon={faUser} /></a>
+            <button className="nav-item nav-link btn btn-link" onClick={() => window.location.href = 'contact.html'}>Contact</button>
+            <button className="nav-item nav-link btn btn-link"><FontAwesomeIcon icon={faUser} /></button>
           </div>
         </div>
       </nav>
@@ -52,7 +52,7 @@ const Register = () => {
               <h1 className="display-3 text-white animated slideInDown">Signup</h1>
               <nav aria-label="breadcrumb">
                 <ol className="breadcrumb justify-content-center">
-                  <li className="breadcrumb-item"><a className="text-white" href="index.html">Home</a></li>
+                  <li className="breadcrumb-item"><button className="btn btn-link text-white" onClick={() => window.location.href = 'index.html'}>Home</button></li>
                   <li className="breadcrumb-item text-white active" aria-current="page">Signup</li>
                 </ol>
               </nav>
@@ -65,7 +65,7 @@ const Register = () => {
       {/* Signup Start */}
       <div className="container-xxl py-2 mt-4">
         <div className="container">
-          <div className="row g-4 wow fadeInUp" data-wow-delay="0.5s ">
+          <div className="row g-4 wow fadeInUp" data-wow-delay="0.5s">
             <center>
               <form className="shadow p-4" style={{ maxWidth: '550px' }}>
                 <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
@@ -94,7 +94,7 @@ const Register = () => {
                     <button className="btn text-light w-100 py-3" type="submit">Signup</button>
                   </div>
                   <div className="col-12 text-center">
-                    <p>Already have an account? <a className="text-decoration-none" href="login.html">Login</a></p>
+                    <p>Already have an account? <button className="btn btn-link text-decoration-none" onClick={() => window.location.href = 'login.html'}>Login</button></p>
                   </div>
                 </div>
               </form>
@@ -110,8 +110,8 @@ const Register = () => {
           <div className="row g-5">
             <div className="col-lg-4 col-md-6">
               <h4 className="text-white mb-3">Quick Link</h4>
-              <p><a className="text-light" href="about.html">About Us</a></p>
-              <p><a className="text-light" href="contact.html">Contact Us</a></p>
+              <p><button className="btn btn-link text-light" onClick={() => window.location.href = 'about.html'}>About Us</button></p>
+              <p><button className="btn btn-link text-light" onClick={() => window.location.href = 'contact.html'}>Contact Us</button></p>
             </div>
             <div className="col-lg-4 col-md-6">
               <h4 className="text-white mb-3">Contact</h4>
@@ -119,10 +119,10 @@ const Register = () => {
               <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>+91 8683045908</p>
               <p className="mb-2"><i className="fa fa-envelope me-3"></i>secretcoder@gmail.com</p>
               <div className="d-flex pt-2">
-                <a className="btn btn-outline-light btn-social" href=""><i className="fab fa-twitter"></i></a>
-                <a className="btn btn-outline-light btn-social" href=""><i className="fab fa-facebook-f"></i></a>
-                <a className="btn btn-outline-light btn-social" href=""><i className="fab fa-youtube"></i></a>
-                <a className="btn btn-outline-light btn-social" href=""><i className="fab fa-linkedin-in"></i></a>
+                <button className="btn btn-outline-light btn-social"><i className="fab fa-twitter"></i></button>
+                <button className="btn btn-outline-light btn-social"><i className="fab fa-facebook-f"></i></button>
+                <button className="btn btn-outline-light btn-social"><i className="fab fa-youtube"></i></button>
+                <button className="btn btn-outline-light btn-social"><i className="fab fa-linkedin-in"></i></button>
               </div>
             </div>
           </div>
@@ -131,7 +131,7 @@ const Register = () => {
           <div className="copyright">
             <div className="row">
               <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                &copy; <a className="border-bottom" href="index.html">Secret Coder</a>, All Right Reserved.
+                &copy; <button className="btn btn-link text-light" onClick={() => window.location.href = 'index.html'}>Secret Coder</button>, All Rights Reserved.
               </div>
             </div>
           </div>
@@ -140,10 +140,9 @@ const Register = () => {
       {/* Footer End */}
 
       {/* Back to Top */}
-      <a href="#" className="btn btn-lg btn-primary btn-lg-square back-to-top"><i className="bi bi-arrow-up"></i></a>
+      <button className="btn btn-lg btn-primary btn-lg-square back-to-top"><i className="bi bi-arrow-up"></i></button>
     </>
   );
 };
 
 export default Register;
-
