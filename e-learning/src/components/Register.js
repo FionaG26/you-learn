80f1aa8585115876f9a964081440a9067ac585de
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-09-28 11:52:02 +0300

    Update Register.js

--- e-learning/src/components/Register.js
+++ e-learning/src/components/Register.js
@@ -39,7 +39,6 @@ const Register = () => {
         <link href="css/style.css" rel="stylesheet" />
       </Helmet>
 
-<body>
     <!-- Spinner Start -->
     <div id="spinner"
         class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
