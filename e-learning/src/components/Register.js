9cae4394a28ae40768c2f85c84561e07d561a64e
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-09-28 11:37:19 +0300

    Update Register.js

--- e-learning/src/components/Register.js
+++ e-learning/src/components/Register.js
@@ -8,23 +8,6 @@ const Register = () => {
     <head>
     <meta charset="utf-8">
     <title>You Learn : Signup</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
-
-  
-    <link href="img/icon.png" rel="icon">
-
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap"
-        rel="stylesheet">
-
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
 </head>
 
 <body>
